abbdf27afd663bc83a344b86e825140122577b56
Trevor Schwab <trevschwab@gmail.com>
2021-01-23 15:46:39 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
+app.get('/', (req, res) => {
+  const articles = [{
+    title: 'Test Article',
+    createdAt: new Date(),
+    description: 'Test Description'
+  }]
+  res.render('articles/index', { articles })
+})
+
+app.listen(5000)
